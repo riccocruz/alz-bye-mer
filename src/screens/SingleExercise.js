59ce2176f4c4e261bd226e557d5d0897132a0343
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2019-03-06 12:43:52 -0800

    loading added to HomeScreen

--- src/screens/SingleExercise.js
+++ src/screens/SingleExercise.js
@@ -1,12 +1,8 @@
 // This is temporary as Ricco is already working on daily challenge.
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import { View, Text } from 'react-native';
-=======
 import { TextInput, Text, View, TouchableOpacity, ScrollView, StyleSheet } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { ListItem } from 'react-native-elements';
->>>>>>> 2e42832dff890b035cc5c5d96342ade4765709f3
 
 export default class SingleExercise extends Component {
   static navigationOptions = {
@@ -18,35 +14,23 @@ export default class SingleExercise extends Component {
     this.state = {
       type: '',
       difficulty: '',
-<<<<<<< HEAD
-=======
       icon: '',
->>>>>>> 2e42832dff890b035cc5c5d96342ade4765709f3
     }
   }
 
   componentWillMount() {
     let type = this.props.navigation.getParam('type');
     let difficulty = this.props.navigation.getParam('difficulty');
-<<<<<<< HEAD
-    if (type == undefined && difficulty == undefined) {
-      type='dailyChallenge';
-      difficulty='dynamic';
-=======
     let icon = this.props.navigation.getParam('icon');
     if (type == undefined && difficulty == undefined) {
       type='dailyChallenge';
       difficulty='dynamic';
       icon="shape";
->>>>>>> 2e42832dff890b035cc5c5d96342ade4765709f3
     }
     this.setState({
       type: type,
       difficulty: difficulty,
-<<<<<<< HEAD
-=======
       icon: icon,
->>>>>>> 2e42832dff890b035cc5c5d96342ade4765709f3
     });
   }
 
@@ -56,17 +40,6 @@ export default class SingleExercise extends Component {
 
   render() {
     return (
-<<<<<<< HEAD
-      <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-        <Text>This is temporary screen as Ricco is already working on this page.</Text>
-        <Text>This page is simply to test game type/difficulty param passed in</Text>
-        <Text style={{color:'red'}}>Game Type: {this.state.type}</Text>
-        <Text style={{color:'red'}}>Difficulty: {this.state.difficulty}</Text>
-      </View>
-    )
-  }
-}
-=======
       <ScrollView style={{flex:1}}>
       <View style={styles.title_container}>
         <MaterialCommunityIcons name = {this.state.icon} size={32} />
@@ -100,4 +73,3 @@ const styles = StyleSheet.create({
 
   },
 });
->>>>>>> 2e42832dff890b035cc5c5d96342ade4765709f3
