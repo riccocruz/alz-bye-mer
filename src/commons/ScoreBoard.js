a53034eb099a0ca4b889d695366f92df039e0f92
Minhyeong Joe <minhyeong.billy.joe@gmail.com>
2019-03-07 19:19:25 -0800

    Billy (#30)
    * Cognitive game boilerplates set-up

---
+++ src/commons/ScoreBoard.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ScoreBoard = ({difficulty, score, stage}) => {
+  return (
+    <View style={styles.scoreboard}>
+      <Text>Difficulty: {difficulty}</Text>
+      <View style={styles.row}>
+        <Text>Score: </Text>
+        <Text style={styles.score_box}>{score}</Text>
+      </View>
+      <View style={styles.row}>
+        <Text>Current Stage: </Text>
+        <Text style={styles.stage_box}>{stage} / 10</Text>
+      </View>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  scoreboard: {
+    width:'100%',
+    flexDirection:'row',
+    justifyContent:'space-around',
+    paddingTop: 15,
+    paddingBottom: 15,
+  },
+  row: {
+    flexDirection:'row'
+  },
+  score_box: {
+    borderWidth: 1,
+    borderColor: 'black',
+    paddingLeft:5,
+    paddingRight:5,
+    width: 25,
+    textAlign: 'center'
+  },
+  stage_box: {
+    borderWidth: 1,
+    borderColor: 'black',
+    paddingLeft:5,
+    paddingRight:5,
+    width: 60,
+    textAlign: 'center'
+  }
+});
+
+export default ScoreBoard;
